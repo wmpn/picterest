06132fa4192144226438342c36974be7bdb157c3
Praveen Nirmal <wmpnga@gmail.com>
2021-05-24 18:47:39 +0530

    changed title appearance as logo 1.0

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>Picterest</h1>
+      <h1><span>p</span>icterest</h1>
       <h2>Your Pictures</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </div>
