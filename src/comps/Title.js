b01eef164d665a24cb8a8962481807393f3c9b4b
Praveen Nirmal <wmpnga@gmail.com>
2021-06-12 15:09:55 +0530

    Edditing dummy description

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1><span>p</span>icterest</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Add your pictures to the online gallery</p>
     </div>
   )
 }
