4ecec08fb2116791cb578dce726daff0399df40e
Praveen Nirmal <wmpnga@gmail.com>
2021-06-20 19:12:12 +0530

    added hooks to get selecting images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/upload';
+import ImageGrid from './comps/imageGrid';
+import Model from './comps/model';
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title />
       <UploadForm />
+      <ImageGrid setSelectedImg={setSelectedImg} />
+      {selectedImg && <Model selectedImg={selectedImg} setSelectedImg={setSelectedImg} />}
     </div>
   );
 }
